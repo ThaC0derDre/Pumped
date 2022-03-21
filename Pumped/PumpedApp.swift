7350212e5c33aad5002b4a61c7937ec0129d9637
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-21 13:09:05 -0700

    Initial Commit

---
+++ Pumped/PumpedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpedApp.swift
+//  Pumped
+//
+//  Created by Andres Gutierrez on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PumpedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
