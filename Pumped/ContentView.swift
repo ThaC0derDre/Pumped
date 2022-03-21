ec60010b6104f743ddb4039ea1b65d42333fade7
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-21 13:26:09 -0700

    initial set up

--- Pumped/ContentView.swift
+++ Pumped/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var lStartTime   = Date()
+    @State private var rStartTime   = Date()
+    @State private var lfDuration   = Date()
+    @State private var rtDuration   = Date()
+    
+    static var defaultTime: Date {
+        var component = DateComponents()
+        component.hour      = 7
+        component.minute    = 0
+        return Calendar.current.date(from: component) ?? Date.now
+    }
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        List {
+            Section {
+                HStack {
+                    Text("Left Side")
+                    
+                    
+                }
+            }
+        }
+        .navigationTitle("Pump")
+        .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
